b9c3a777f96d5edde94f401d99d4156fca56222b
FINEARCHS <133759614+FineArchs@users.noreply.github.com>
2026-02-19 13:48:28 +0900

    Update CORS headers to allow all origins

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ addEventListener("fetch", async event => {
 
         // Function to modify headers to enable CORS
         function setupCORSHeaders(headers) {
-            headers.set("Access-Control-Allow-Origin", event.request.headers.get("Origin"));
+            // headers.set("Access-Control-Allow-Origin", event.request.headers.get("Origin"));
+            headers.set("Access-Control-Allow-Origin", "*");
             if (isPreflightRequest) {
                 headers.set("Access-Control-Allow-Methods", event.request.headers.get("access-control-request-method"));
                 const requestedHeaders = event.request.headers.get("access-control-request-headers");
